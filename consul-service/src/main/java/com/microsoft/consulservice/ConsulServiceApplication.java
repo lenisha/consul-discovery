1e15bfedc7b92ee2df891effa44c1dcb155e5dfa
Elena Neroslavskaya <lenisha@gmail.com>
2018-11-18 16:18:31 -0500

    init

---
+++ consul-service/src/main/java/com/microsoft/consulservice/ConsulServiceApplication.java
@@ -0,0 +1,20 @@
+package com.microsoft.consulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConsulServiceApplication {
+
+	@RequestMapping("/hello")
+    public String home() {
+        return "Hello world";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulServiceApplication.class, args);
+	}
+}
